dd4e031d4f8c2dd2f3e8fededd6b8e72c5275a90
potato-csh <1103782026@qq.com>
2021-07-16 19:50:15 +0800

    test

--- test.py
+++ test.py
@@ -1,20 +1,3 @@
 #!/usr/bin/env python
 # coding:utf-8
 
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return 'hello'
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9902)
-
-# test
-# test
-# test
-
